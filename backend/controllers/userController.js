68ec6ef372dc752d1d3867b4ccfa46c10e1f9477
Vinik <56496329+Vn1k@users.noreply.github.com>
2024-12-24 01:16:35 +0700

    Update user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,6 +10,23 @@ exports.createUser = async (req, res) => {
   }
 };
 
+exports.updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, email, phone, address } = req.body;
+    const [updated] = await users.update({name, email, phone, address}, {
+      where: { id }
+    });
+    if (!updated) {
+      return res.status(404).json({ message: "User not found: " });
+    }
+    const updatedUser = await users.findOne({ where: { id } });
+    return res.status(200).json({ message: "user updated"+ id,user: updatedUser });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
 exports.getAllUsers = async (req, res) => {
     try {
       const usersList = await users.findAll();

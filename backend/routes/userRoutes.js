35ba940a4129bcb4c849fa48032d4a5b54eee862
Vinik <56496329+Vn1k@users.noreply.github.com>
2024-12-24 01:17:14 +0700

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/user/create', userController.createUser);
+router.put('/user/update/:id', userController.updateUser);
 router.get('/users', userController.getAllUsers);
 
 module.exports = router;
